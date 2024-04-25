44f549fb5fd34c484d24603f73f735bf36e1a461
Arnab Mondal <dp950810@gmail.com>
2024-04-25 18:48:38 +0530

    Update arrsatruct.c

--- DMA/arrsatruct.c
+++ DMA/arrsatruct.c
@@ -1,30 +1,49 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(int argc, char** argv)
-{
-    typedef struct
-    {
-        char* firstName;
-        char* lastName;
-        int rollNumber;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    } STUDENT;
+typedef struct {
+    char* firstName;
+    char* lastName;
+    int rollNumber;
+} STUDENT;
 
-    int numStudents=1;
+int main() {
+    int numStudents = 1;
     int x;
-    STUDENT* students = malloc(numStudents * sizeof (*students));
-   
-    for (x = 0; x < numStudents; x++)
-    {
-        students[x].firstName=(char*)malloc(sizeof(char*));
-       
-        printf("Enter first name :");
-        scanf("%s",students[x].firstName);
-        students[x].lastName=(char*)malloc(sizeof(char*));
-        printf("Enter last name :");
-        scanf("%s",students[x].lastName);
-        printf("Enter roll number  :");
-        scanf("%d",students[x].rollNumber);
 
+    STUDENT* students = malloc(numStudents * sizeof(STUDENT));
+
+    if (students == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
     }
+
+    for (x = 0; x < numStudents; x++) {
+        char input[100]; // Assuming max length of input string is 100 characters
+
+        printf("Enter first name: ");
+        scanf("%s", input);
+        students[x].firstName = malloc(strlen(input) + 1); // Allocate memory for string and null terminator
+        strcpy(students[x].firstName, input);
+
+        printf("Enter last name: ");
+        scanf("%s", input);
+        students[x].lastName = malloc(strlen(input) + 1); // Allocate memory for string and null terminator
+        strcpy(students[x].lastName, input);
+
+        printf("Enter roll number: ");
+        scanf("%d", &students[x].rollNumber);
+
+        printf("Student details: %s %s %d\n", students[x].firstName, students[x].lastName, students[x].rollNumber);
+    }
+
+    // Free allocated memory
+    for (x = 0; x < numStudents; x++) {
+        free(students[x].firstName);
+        free(students[x].lastName);
+    }
+    free(students);
+
+    return 0;
 }
